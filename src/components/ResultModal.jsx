d6237fa4b5b67d09daa4163447609cd6f838a262
Luka <luka.luka.luka93@gmail.com>
2025-03-04 11:47:31 -0800

    adding portal via create portal to put it inside the div in a body

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -3,12 +3,14 @@
 // for older versions of React 19:
 
 import {forwardRef, useImperativeHandle, useRef} from 'react';
+import { createPortal } from 'react-dom';
 
-const ResultModal = forwardRef(function ResultModal({remainingTime, targetTime, onReset}, ref) {
+const ResultModal = forwardRef(function ResultModal({remainingTime, onReset, targetTime}, ref) {
     const dialog = useRef();
 
     const userLost = remainingTime <= 0;
-    const formattedRemainingTime = (remainingTime / 1000).toFixed(2)
+    const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+    const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
 
     useImperativeHandle(ref, ()=> {
         return {
@@ -18,15 +20,17 @@ const ResultModal = forwardRef(function ResultModal({remainingTime, targetTime, 
         }
     })
 
-    return (
-        <dialog className='result-modal' ref={dialog}>
-        {userLost && <h2>You lost!</h2>}
+    return createPortal(
+        <dialog className='result-modal' ref={dialog} onClose={onReset}>
+        {userLost && <h2>You Lost!</h2>}
+        {!userLost && <h2>Your Score: {score}</h2>}
         <p>The target time was <strong>{targetTime} seconds.</strong></p>
         <p>You stopped the timer with <strong>{formattedRemainingTime} seconds left.</strong></p>
         <form method='dialog' onSubmit={onReset}>
             <button>Close</button>
         </form>
-    </dialog>
+    </dialog>,
+    document.getElementById('modal')
     )
 })
 
